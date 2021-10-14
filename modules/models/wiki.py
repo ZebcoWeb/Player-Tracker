26a888e3f7abf584ce7da216890fb8265b6d1bf5
Zebco <zebco1382@gmail.com>
2021-10-14 18:45:52 +0330

    create model orm wiki

---
+++ modules/models/wiki.py
@@ -0,0 +1,26 @@
+import datetime
+
+from mongoengine import fields, Document, NULLIFY
+
+
+
+
+class WikiModel(Document):
+    name = fields.StringField(required=True, unique=True)
+    game = fields.ReferenceField('GameModel', reverse_delete_rule=NULLIFY, default=None)
+    logo_path = fields.StringField(required=True)
+    banner_path = fields.StringField(default=None)
+    url = fields.URLField(required=True)
+    visit_value = fields.IntField(default=0)
+    is_active = fields.BooleanField(default=True)
+    created_at = fields.DateTimeField()
+
+    meta = {
+        'ordering': ['-visit_value'],
+        'collection': 'Wiki',
+    }
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.now()
+        return super(WikiModel, self).save(*args, **kwargs)
