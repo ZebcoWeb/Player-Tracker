2daf301a21f7dafcb54a77e548d5f1c5f912792f
Zebco <zebco1382@gmail.com>
2021-10-20 11:38:13 +0330

    add load extensions to run.py

--- run.py
+++ run.py
@@ -3,18 +3,33 @@ import os
 from discord import Intents
 from discord.ext.commands import Bot
 
-
+from modules.config import Env
 
 
 
 def run_discord_client():
+    
     client = Bot(
         help_command=None,
         command_prefix='!',
         intents=Intents().all()
     )
+    
+    loaded_extensions = {}
 
-    client.run('ODU0NjQ3Mjc5Njc1Mzc1NjM2.YMm-Tg.iIOkqDmjlUxY1B5c86IQAq8PHsg')
+    for path, subdirs, files in os.walk('cogs/'):
+        for name in files:
+            if name.endswith('.py'):
+                filename = os.path.join(path, name).replace('/', '.').replace('\\', '.')[:-3]
+                cog_name = name[:-3].split('.')[-1]
+                
+                loaded_extensions[cog_name] = filename
+
+                client.load_extension(filename)
+
+                print(f"\n- Loaded {filename}")
+
+    client.run(Env.TOKEN)
 
 
 
