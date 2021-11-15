4eb433e44e97b8d6f8102eba29842f0027955462
ZebcoWeb <zebco1382@gmail.com>
2021-11-15 21:21:16 +0330

    fix some problems in load extensions

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ from discord.ext.commands import Bot
 from modules.config import Env
 
 
-
 def run_discord_client():
     
     client = Bot(
@@ -14,20 +13,16 @@ def run_discord_client():
         command_prefix='!',
         intents=Intents().all()
     )
-    
-    loaded_extensions = {}
 
     for path, subdirs, files in os.walk('cogs/'):
         for name in files:
             if name.endswith('.py'):
                 filename = os.path.join(path, name).replace('/', '.').replace('\\', '.')[:-3]
-                cog_name = name[:-3].split('.')[-1]
-                
-                loaded_extensions[cog_name] = filename
 
                 client.load_extension(filename)
-
-                print(f"\n- Loaded {filename}")
+        
+    
+    print('Loaded extensions --> ' + ', '.join(client.extensions.keys()))
 
     client.run(Env.TOKEN)
 
