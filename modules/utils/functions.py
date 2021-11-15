a8cb1fd3150583c7cdd23e4735f19d3ca9deff97
ZebcoWeb <zebco1382@gmail.com>
2021-11-15 21:19:03 +0330

    create Attach class

--- modules/utils/functions.py
+++ modules/utils/functions.py
@@ -1,10 +1,35 @@
+import os
+
 from datetime import datetime
 
-
 def time_diff_min(start_time: datetime, end_time: datetime = datetime.now()):
-    '''Returns the distance between two Datetimes in minutes
-    '''
+    """Returns the distance between two Datetimes in minutes
+    """
     
     if start_time <= end_time:
         return round((end_time - start_time).total_seconds() / 60.0)
     return 0
+
+
+class Attach:
+    """Construct a Attach for local files"""
+    def __init__(self, filepath) -> None:
+        self.filepath = filepath
+
+    @property
+    def filename(self):
+        return self.filepath.split('/')[-1]
+    
+    @property
+    def url(self):
+        return 'attachment://' + self.filename
+    
+    @property
+    def size(self):
+        """Return file sixe to KB"""
+        return round(
+            number = os.path.getsize(self.filepath) / 1024, 
+            ndigits=2
+        )
+
+
