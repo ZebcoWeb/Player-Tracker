4cf3c8e8d86836b98bdd1ffa61abf2e200f56511
ZebcoWeb <zebco1382@gmail.com>
2021-11-20 20:11:41 +0330

    create special funcs for each operation

--- run.py
+++ run.py
@@ -1,19 +1,27 @@
 import os
 
 from discord import Intents
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, when_mentioned_or
 
+from modules.database import signals
 from modules.config import Env
+from modules.utils import get_logger, LogType
 
 
-def run_discord_client():
-    
-    client = Bot(
-        help_command=None,
-        command_prefix='!',
-        intents=Intents().all()
+
+def load_persistent_views(client):
+    list_view_classes = [
+        #! list presistent view here
+    ]
+    if list_view_classes.count() !=0:
+        for view in list_view_classes:
+            client.add_view(view)
+    client.persistent_views_added = True
+    get_logger(LogType.DEBUG).debug(
+        'Presistent views --> ' + ', '.join(list_view_classes)
     )
 
+def load_extentions(client):
     for path, subdirs, files in os.walk('cogs/'):
         for name in files:
             if name.endswith('.py'):
@@ -21,12 +29,36 @@ def run_discord_client():
 
                 client.load_extension(filename)
         
-    
-    print('Loaded extensions --> ' + ', '.join(client.extensions.keys()))
+    get_logger(LogType.DEBUG).debug(
+        'Loaded extensions --> ' + ', '.join(client.extensions.keys())
+    )
+
+
+def run_discord_client():
+
+    class PPClient(Bot):
+        def __init__(self):
+            super().__init__(
+                help_command = None,
+                command_prefix = when_mentioned_or('!'),
+                Intents = Intents().all(),
+                persistent_views_added = False,
+                )
+        
+        async def on_ready(self):
+
+            get_logger(LogType.DEBUG).debug('Loading persistent views...')
+            load_persistent_views(self)
+
+            get_logger(LogType.DEBUG).debug('Loading extentions...')
+            load_extentions(self)
+
+            get_logger(LogType.PT).debug('Player Tracker is online!')
+
+
+    client = PPClient()
 
     client.run(Env.TOKEN)
-
-
 
 if __name__ == '__main__':
     run_discord_client()
