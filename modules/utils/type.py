3541f87a14b74e01f0c41607f3ea34805dee37a9
ZebcoWeb <zebco1382@gmail.com>
2021-11-21 20:16:16 +0330

    replace type instead of enum

---
+++ modules/utils/type.py
@@ -0,0 +1,26 @@
+__all__ = ['LogType']
+
+class LogType:
+
+    Cache = 'CATCH'
+    Database = 'DATABASE'
+    PT = 'PLAYER-TRACKER'
+    Env = 'ENV'
+    Yaml = 'YAML'
+    Discord = 'DISCORD'
+    Test = 'TEST'
+    PT = 'PLAYER TRACKER'
+    DEBUG = 'DEBUG'
+
+
+class Room:
+
+    class Mode:
+        PRIVATE = ''
+        PUBLIC = ''
+
+    class Bitrate:
+        pass
+
+    class Capacity:
+        pass
