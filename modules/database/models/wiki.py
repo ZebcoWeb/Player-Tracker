b00684f45351fb7ca12087c0071163d56f6e1f36
ZebcoWeb <zebco1382@gmail.com>
2021-11-21 20:20:55 +0330

    set index meta option to meld

--- modules/database/models/wiki.py
+++ modules/database/models/wiki.py
@@ -1,7 +1,12 @@
+import aiohttp
+import asyncio
+
 from datetime import datetime
 
 from mongoengine import fields, Document, NULLIFY
+from aiowiki import Wiki
 
+from modules.utils import Attach
 from .game import GameModel
 
 
@@ -11,7 +16,8 @@ class WikiModel(Document):
     game = fields.ReferenceField('GameModel', reverse_delete_rule=NULLIFY, default=None)
     logo_path = fields.StringField(required=True)
     banner_path = fields.StringField(default=None)
-    url = fields.URLField(required=True)
+    parent_site = fields.StringField(default='fandom.com')
+    subdomain = fields.StringField(required=True)
     visit_value = fields.IntField(default=0)
     is_active = fields.BooleanField(default=True)
     created_at = fields.DateTimeField()
@@ -19,9 +25,56 @@ class WikiModel(Document):
     meta = {
         'ordering': ['-visit_value'],
         'collection': 'Wiki',
+        'indexes': [
+            'name',  # text index
+        ]
     }
 
     def save(self, *args, **kwargs):
         if not self.created_at:
             self.created_at = datetime.now()
         return super(WikiModel, self).save(*args, **kwargs)
+    
+
+    @property
+    def attach_logo_path(self):
+        return Attach(self.logo_path)
+
+    @property
+    def attach_banner_path(self):
+        return Attach(self.banner_path) if self.banner_path else None
+    
+    @property
+    def link(self):
+        return 'https://' + self.subdomain + f'.{self.parent_site}'
+
+    @property
+    def api_link(self):
+        return self.link + '/api.php'
+
+
+    @property
+    async def status_code(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_link) as response:
+                return response.status
+
+    @property
+    def is_online(self):
+        status_code = asyncio.run(self.status_code)
+
+        return True if status_code == 200 else False
+        
+    @property
+    def wiki(self):
+        if self.is_active:
+            if self.is_online:
+                return Wiki(base_url=self.api_link)
+            else:
+                pass
+            # TODO: riase error for not online
+        else:
+            pass
+        # TODO: raise error for not active
+    
+    
