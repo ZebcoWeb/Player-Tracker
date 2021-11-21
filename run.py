a9916dc8d1f70b50394cb4ac686754be3c4cda28
ZebcoWeb <zebco1382@gmail.com>
2021-11-21 20:19:00 +0330

    remove import logtype

--- run.py
+++ run.py
@@ -1,27 +1,45 @@
 import os
 
-from discord import Intents
+from discord import Intents, Client
 from discord.ext.commands import Bot, when_mentioned_or
 
-from modules.database import signals
 from modules.config import Env
 from modules.utils import get_logger, LogType
+from data.config import Config
 
+class PPClient(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            help_command = None,
+            command_prefix = when_mentioned_or('!'),
+            Intents = Intents().all(),
+            )
 
+    async def on_ready(self):
+        get_logger(LogType.PT).debug('Player Tracker is online!')
+        print('Bot is run!')
 
-def load_persistent_views(client):
-    list_view_classes = [
-        #! list presistent view here
-    ]
-    if list_view_classes.count() !=0:
-        for view in list_view_classes:
+def run_discord_client():
+
+    client = PPClient()
+
+    # Load presistent views
+    print('Loading persistent views...')
+    if len(Config.list_view_classes) != 0:
+        for view in Config.list_view_classes:
             client.add_view(view)
-    client.persistent_views_added = True
-    get_logger(LogType.DEBUG).debug(
-        'Presistent views --> ' + ', '.join(list_view_classes)
-    )
+        client.persistent_views_added = True
 
-def load_extentions(client):
+        get_logger(LogType.DEBUG).debug(
+            'Presistent views --> ' + ', '.join(Config.list_view_classes)
+        )
+    else:
+        get_logger(LogType.DEBUG).debug(
+            'No views loaded'
+        )
+
+    # Load extentions
+    print('Loading extentions...')
     for path, subdirs, files in os.walk('cogs/'):
         for name in files:
             if name.endswith('.py'):
@@ -33,35 +51,7 @@ def load_extentions(client):
         'Loaded extensions --> ' + ', '.join(client.extensions.keys())
     )
 
-
-def run_discord_client():
-
-    class PPClient(Bot):
-        def __init__(self):
-            super().__init__(
-                help_command = None,
-                command_prefix = when_mentioned_or('!'),
-                Intents = Intents().all(),
-                persistent_views_added = False,
-                )
-        
-        async def on_ready(self):
-
-            get_logger(LogType.DEBUG).debug('Loading persistent views...')
-            load_persistent_views(self)
-
-            get_logger(LogType.DEBUG).debug('Loading extentions...')
-            load_extentions(self)
-
-            get_logger(LogType.PT).debug('Player Tracker is online!')
-
-
-    client = PPClient()
-
-    client.run(Env.TOKEN)
+    return client.run(Env.TOKEN) # Run Client
 
 if __name__ == '__main__':
     run_discord_client()
-
-
-
