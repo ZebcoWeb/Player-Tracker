611d58639f164fb57d140f2e46121a059f93f8df
ZebcoWeb <zebco1382@gmail.com>
2021-11-25 01:28:06 +0330

    add todoist token

--- modules/config/env.py
+++ modules/config/env.py
@@ -8,11 +8,13 @@ class Env:
     load_dotenv('data/.env')
 
     TOKEN = str(getenv('TOKEN'))
+    TODOIST_TOKEN = str(getenv('TODOIST_TOKEN'))
     
     DATABASE_HOST = str(getenv('DATABASE_HOST'))
     DATABASE_DB = str(getenv('DATABASE_DB'))
     DATABASE_USER = str(getenv('DATABASE_USER'))
     DATABASE_PASSWORD = str(getenv('DATABASE_PASSWORD'))
+
 
     CACHE_HOST = str(getenv('CACHE_HOST'))
     CACHE_USER = str(getenv('CACHE_USER'))
