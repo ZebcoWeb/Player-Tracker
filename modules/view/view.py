7fed8e99ff7727669e4fd3863c3cdd19f76d8e70
ZebcoWeb <zebco1382@gmail.com>
2021-12-25 10:15:18 +0330

    - add kwargs for the range of arguments - add auto update dict

--- modules/view/view.py
+++ modules/view/view.py
@@ -2,9 +2,7 @@ import discord
 
 from discord.ext import commands
 
-
 class PersistentView(discord.ui.View):
-    def __init__(self, client: commands.Bot):
+    def __init__(self, **kwargs):
         super().__init__(timeout=None)
-        client.add_view(self)
-        client.persistent_views_added = True
+        self.__dict__.update(kwargs)
