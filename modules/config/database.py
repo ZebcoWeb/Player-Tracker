1ce9f7f2cc0261aa62cf7313358fb1d8f16fca6c
ZebcoWeb <zebco1382@gmail.com>
2021-12-27 19:53:12 +0330

    - add init db func for beanie

--- modules/config/database.py
+++ modules/config/database.py
@@ -1,12 +1,41 @@
-from mongoengine import connect
+import sys, motor
 
+from beanie import init_beanie
+
+from modules.utils import inspect_models
 from modules.config import Env
-from modules.utils import get_logger, LogType
 
 
+__all__ = ['init_database']
 
-connect(
-        Env.DATABASE_DB, 
-        host=Env.DATABASE_HOST,
-        port=27017
+
+async def init_database():
+    
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        Env.DATABASE_HOST,
+        27017
     )
+    
+    try:
+        info = await client.server_info()
+
+        if info['ok'] == 1.0:
+
+            models = inspect_models()
+
+            await init_beanie(
+                database= client[Env.DATABASE_DB],
+                document_models= models
+            )
+            print(' └ Database is alive. MongoDB version: ' + info['version'] + f'\n └ {len(models)} models were activated in the database!')
+            
+        else:
+            print('└ Database is not alive!')
+            sys.exit(1)
+
+    except Exception as e:
+
+        print('> Could not connect to database!')
+        print('> Exiting...')
+        sys.exit(1)
+
