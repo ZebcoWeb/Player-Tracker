67bc476246df7ad00efba582a8d1c33eeae54b7b
ZebcoWeb <zebco1382@gmail.com>
2021-12-29 10:01:20 +0330

    add RoomCapacity, RoomBitrate, RoomMode, RoomConfirm and other

--- modules/view/room_view.py
+++ modules/view/room_view.py
@@ -1,11 +1,11 @@
+import asyncio
+
 import discord
 from discord import ButtonStyle, Client, Colour
-from discord.interactions import Interaction
-from discord.ui import view
 
 from data.config import Category, Config, Emoji, Role
 from modules.database import GameModel, MemberModel, RoomModel
-from modules.utils import i18n, space
+from modules.utils import i18n
 
 from .view import PersistentView
 
@@ -39,11 +39,7 @@ class CreateRoomView(PersistentView):
                 reason = f'Creating a new room channel by {user.name.lower()}'
             )
 
-            if member_model.lang:
-                locate = i18n('room', member_model.lang.lower()) 
-            else:
-                locate = i18n('room', 'en') # default is 'en'
-            locate.install()
+            locate = i18n() # default is 'en'
             _ = locate.gettext
 
             em = discord.Embed(
@@ -53,11 +49,15 @@ class CreateRoomView(PersistentView):
 
             room_model = RoomModel(creator=member_model)
 
+            room_model.room_create_channel_id = channel.id
+
             form = await channel.send(
                 content=user.mention,
                 embed=em,
                 view=CreateRoomChooseLang(room_model, self.client)
             )
+
+
             view = discord.ui.View()
             button = discord.ui.Button(
                 label='Click here',
@@ -89,6 +89,7 @@ class CreateRoomChooseLang(discord.ui.View):
                 label=name,
                 value=key,
                 emoji=emoji,
+                default=False
             )
         )
 
@@ -97,11 +98,12 @@ class CreateRoomChooseLang(discord.ui.View):
         
         
         self.lang = select.values[0]
-        self.room_model.lang = self.lang
 
         next_button: discord.ui.Button = discord.utils.get(
             self.children, custom_id='choose_next_lang_button'
         )
+
+        self.room_model.lang = self.lang # set lang
 
         next_button.disabled = False
         
@@ -113,7 +115,7 @@ class CreateRoomChooseLang(discord.ui.View):
 
         await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label=space('Next', 3), custom_id='choose_next_lang_button',disabled=True ,style=ButtonStyle.green, emoji=Emoji.ARROW_FORWARD)
+    @discord.ui.button(label='ㅤNextㅤ', custom_id='choose_next_lang_button',disabled=True ,style=ButtonStyle.green, emoji=Emoji.ARROW_FORWARD)
     async def choose_lang_next(self, button: discord.ui.Button, interaction: discord.Interaction):
         if self.lang:
             em = discord.Embed(
@@ -142,37 +144,6 @@ class CreateRoomChooseGame(discord.ui.View):
 
         self.add_item(GamesListSelectMenu(self.client))
         self.add_item(CreateRoomChooseGameNext())
-
-class CreateRoomChooseGameNext(discord.ui.Button):
-    def __init__(self):
-        super().__init__(label='Next', custom_id='choose_next_game_button', 
-        disabled=True, style=ButtonStyle.green, emoji=Emoji.ARROW_FORWARD
-    )
-
-    async def callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-
-        data = discord.utils.get(
-            self.view.children, custom_id='choose_games_select'
-        )
-        game = data.values[0]
-
-        if game not in ['next_page', 'previous_page']:
-
-            self.view.room_model.game = GameModel.get(game)
-
-            em = discord.Embed(
-                description='CONTEXT_CHOOSE_ROOM_GAME_DES',
-                color = Colour.green()
-            )
-            em.set_author(name='CONTEXT_CHOOSE_ROOM_GAME_HEADER')
-            em.set_footer(text='◇──◇──◇──◇──◇──◇')
-
-            # view = CreateRoomChooseName(self.client, self.room_model)
-
-            # await interaction.response.edit_message(
-            #     embed = em,
-            #     view = view
-            # )
 
 
 class GamesListSelectMenu(discord.ui.Select):
@@ -288,7 +259,42 @@ class GamesListSelectMenu(discord.ui.Select):
         await interaction.response.edit_message(view=view)
 
 
-class CreateRoomChooseCapacity:
+class CreateRoomChooseGameNext(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label='ㅤNextㅤ', custom_id='choose_next_game_button', 
+        disabled=True, style=ButtonStyle.green, emoji=Emoji.ARROW_FORWARD
+    )
+
+    async def callback(self, interaction: discord.Interaction):
+
+        view = self.view
+
+        data = discord.utils.get(
+            view.children, custom_id='choose_games_select'
+        )
+        game = data.values[0]
+
+        if game not in ['next_page', 'previous_page']:
+
+            view.room_model.game = GameModel.get(game)
+            view.room_model.start_msg_id = interaction.message.id
+
+            em = discord.Embed(
+                description='CONTEXT_CHOOSE_ROOM_CAPACITY_DES',
+                color = Colour.green()
+            )
+            em.set_author(name='CONTEXT_CHOOSE_ROOM_CAPACITY_HEADER')
+            em.set_footer(text='◇──◇──◇──◇──◇──◇')
+
+            view = CreateRoomChooseCapacity(view.client, view.room_model)
+
+            await interaction.response.edit_message(
+                embed = em,
+                view = view
+            )
+
+
+class CreateRoomChooseCapacity(discord.ui.View):
     def __init__(self, client, room_model):
 
         self.room_model: RoomModel = room_model
@@ -296,3 +302,260 @@ class CreateRoomChooseCapacity:
 
         super().__init__(timeout=None)
     
+
+        counter = 1
+        for i in Config.ROOM_CAPACITIES:
+
+            button = discord.ui.Button(
+                style=ButtonStyle.gray,
+                custom_id = i,
+                emoji = self.client.get_emoji(Config.ROOM_CAPACITIES.get(i).get('emoji')),
+                row=0
+            )
+            if counter > 4: # create a new row
+                button.row += 1
+    
+            counter += 1
+
+            button.callback = self.callback
+            self.add_item(button)
+    
+    async def callback(self, interaction: discord.Interaction):
+            
+        value = interaction.data.get('custom_id')
+        capacity = Config.ROOM_CAPACITIES.get(value).get('capacity')
+
+        self.room_model.capacity = capacity
+
+        em = discord.Embed(
+            description='CONTEXT_CHOOSE_ROOM_BITRATE_DES',
+            color = Colour.green()
+        )
+        em.set_author(name='CONTEXT_CHOOSE_ROOM_BITRATE_HEADER')
+        em.set_footer(text='◇──◇──◇──◇──◇──◇')
+
+        view = CreateRoomChooseBitrate(self.client, self.room_model)
+
+        await interaction.response.edit_message(
+            embed = em,
+            view = view
+        )
+
+# TODO: add region
+# TODO: is waiting room
+class CreateRoomChooseBitrate(discord.ui.View):
+    def __init__(self, client, room_model):
+
+        self.room_model: RoomModel = room_model
+        self.client: Client = client
+
+        super().__init__(timeout=None)
+    
+        for i in Config.ROOM_BITRATES:
+            
+            button = discord.ui.Button(
+                style=ButtonStyle.gray,
+                label = 'kb',
+                custom_id = i,
+                emoji = self.client.get_emoji(Config.ROOM_BITRATES.get(i).get('emoji')),
+            )
+
+            button.callback = self.callback
+            self.add_item(button)
+    
+    async def callback(self, interaction: discord.Interaction):
+
+        value = interaction.data.get('custom_id')
+        bitrate = Config.ROOM_BITRATES.get(value).get('bitrate')
+
+        self.room_model.bitrate = bitrate
+        em = discord.Embed(
+            description='CONTEXT_CHOOSE_ROOM_MODE_DES',
+            color = Colour.green()
+        )
+        em.set_author(name='CONTEXT_CHOOSE_ROOM_MODE_HEADER')
+        em.set_footer(text='◇──◇──◇──◇──◇──◇')
+
+        view = CreateRoomChooseMode(self.client, self.room_model)
+
+        await interaction.response.edit_message(
+            embed = em,
+            view = view
+        )
+class CreateRoomChooseMode(discord.ui.View):
+    def __init__(self, client, room_model):
+
+        self.room_model: RoomModel = room_model
+        self.client: Client = client
+
+        super().__init__(timeout=None)
+    
+        for i in Config.ROOM_MODES:
+            
+            button = discord.ui.Button(
+                style=Config.ROOM_MODES.get(i).get('style'),
+                label = Config.ROOM_MODES.get(i).get('i18n'),
+                custom_id = i,
+                emoji = self.client.get_emoji(Config.ROOM_MODES.get(i).get('emoji')),
+            )
+
+            button.callback = self.callback
+            self.add_item(button)
+    
+    async def callback(self, interaction: discord.Interaction):
+
+        value = interaction.data.get('custom_id')
+        mode = Config.ROOM_MODES.get(value).get('mode')
+
+        self.room_model.mode = mode
+        print(self.room_model.__dict__)
+        em = discord.Embed(
+            description='CONTEXT_CHOOSE_ROOM_CONFIRM_DES',
+            color = Colour.green()
+        )
+        em.set_author(name='CONTEXT_CHOOSE_ROOM_CONFIRM_HEADER')
+        em.set_footer(text='◇──◇──◇──◇──◇──◇')
+
+        view = CreateRoomConfirm(self.client, self.room_model)
+
+        await interaction.response.edit_message(
+            embed = em,
+            view = view
+        )
+
+class CreateRoomConfirm(discord.ui.View):
+    def __init__(self, client, room_model):
+
+        self.room_model: RoomModel = room_model
+        self.client: Client = client
+
+        super().__init__(timeout=None)
+
+        self.add_item(RoomConfirmButton(
+            emoji=self.client.get_emoji(Emoji.CONFIRM_TONE_ID),
+        ))
+
+        self.add_item(RoomAgainButton(
+            emoji=self.client.get_emoji(Emoji.AGAIN_TONE_ID),
+        ))
+
+        self.add_item(RoomCancelButton(
+            emoji=self.client.get_emoji(Emoji.CABCEL_TONE_ID),
+        ))
+
+
+class RoomConfirmButton(discord.ui.Button):
+    def __init__(self, emoji: Emoji):
+        super().__init__(
+            label='Confirm',
+            custom_id='confirm_button',
+            style=ButtonStyle.green,
+            emoji=emoji,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        pass
+
+class RoomAgainButton(discord.ui.Button):
+    def __init__(self, emoji: Emoji):
+        super().__init__(
+            label='Again',
+            custom_id='again_button',
+            style=ButtonStyle.blurple,
+            emoji=emoji,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+
+        room_model: RoomModel = self.view.room_model
+        client = self.view.client
+        new_room_model = RoomModel(
+            creator = room_model.creator,
+            start_msg_id = room_model.start_msg_id,
+            room_create_channel_id = room_model.room_create_channel_id,
+            room_again_created = True
+        )
+        
+        del room_model
+        
+        locate = i18n() # default is 'en'
+        _ = locate.gettext
+
+        em = discord.Embed(
+                description=_(f'CONTEXT_CHOOSE_ROOM_LANG_DES\n\n{Emoji.TEST}'),
+                color = Colour.green()
+        )
+        em.set_footer(text='◇──◈──◇──◇──◇──◇')
+
+        view = CreateRoomChooseLang(client, new_room_model)
+
+        await interaction.response.edit_message(
+            embed = em,
+            view = view
+        )
+
+class RoomCancelButton(discord.ui.Button):
+    def __init__(self, emoji: Emoji):
+        super().__init__(
+            label='Cancel',
+            custom_id='cancel_button',
+            style=ButtonStyle.red,
+            emoji=emoji,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        em = discord.Embed(
+            description='CANCEL_ROOM_DES',
+            color = Colour.red()
+        )
+        em.set_author(name='CANCEL_ROOM_HEADER')
+
+        await interaction.response.edit_message(
+            embed=em, 
+            view=RoomCloseCancelView(self.view.client, self.view.room_model)
+        )
+
+class RoomCloseCancelView(discord.ui.View):
+    def __init__(self, client, room_model):
+
+        self.room_model: RoomModel = room_model
+        self.client: Client = client
+
+        super().__init__(timeout=None)
+
+        button_yes = discord.ui.Button(
+                label= 'ㅤㅤㅤYESㅤㅤㅤ',
+                style=ButtonStyle.green,
+                custom_id='close_room_yes',
+        )
+        button_yes.callback = self.callback_yes
+
+        button_no = discord.ui.Button(
+                label= 'NO',
+                style=ButtonStyle.red,
+                custom_id='close_room_no',
+
+        )
+        button_no.callback = self.callback_no
+        
+
+        self.add_item(button_yes)
+        self.add_item(button_no)
+
+    async def callback_yes(self, interaction: discord.Interaction):
+        em = discord.Embed(
+            description='CLOSE_ROOM_DES',
+            color = Colour.red()
+        )
+        em.set_author(name='CLOSE_ROOM_HEADER')
+
+        await interaction.response.edit_message(embed=em, view=None)
+        channel = await self.client.fetch_channel(self.room_model.room_create_channel_id)
+        
+        await asyncio.sleep(10)
+
+        await channel.delete()
+        del self.room_model
+
+    async def callback_no(self, interaction: discord.Interaction):
+        pass
