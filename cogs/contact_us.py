2d37191acee85d021fd3a7428065edc41336da02
ZebcoWeb <zebco1382@gmail.com>
2022-04-09 19:38:06 +0430

    write contact us section

---
+++ cogs/contact_us.py
@@ -0,0 +1,53 @@
+import discord
+import asyncio
+
+from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Group
+
+from data.config import Config, Category
+from modules.view import ContactUsForm
+from modules.utils import success_embed, error_embed
+
+
+class ContactUs(commands.Cog):
+    def __init__(self, client:commands.Bot):
+        self.client = client
+
+    @app_commands.command(name='contact', description='💬 Contact us')
+    async def contact_us(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(ContactUsForm(self.client))
+
+
+    @app_commands.command(name='close', description='Close ticket')
+    @app_commands.describe(channel='Channel to close the ticket (Optional)')
+    async def contact_us(self, interaction: discord.Interaction, channel: discord.TextChannel = None):
+        if channel is None:
+            channel = interaction.channel
+
+        ticket_category = await self.client.fetch_channel(Category.TICKETS)
+
+        if channel in ticket_category.channels:
+            em = discord.Embed(
+                title="Thanks for giving your time!",
+                description='We are closing this ticket automatically in 10 seconds...',
+                color=Config.BRAND_COLOR
+            )
+            await interaction.response.defer()
+            message = await channel.send(embed=em)
+            await message.add_reaction('🇹')
+            await asyncio.sleep(1)
+            await message.add_reaction('🇭')
+            await asyncio.sleep(1)
+            await message.add_reaction('🇽')
+            await asyncio.sleep(8)
+            await channel.delete()
+        else:
+            await interaction.response.send_message(
+                embed=error_embed(f'Ticket channel not found'), 
+                ephemeral=True
+            )
+
+
+async def setup(client:commands.Bot):
+    await client.add_cog(ContactUs(client))
---
+++ modules/view/contact_modal.py
@@ -0,0 +1,75 @@
+import discord
+
+from discord.ui import TextInput
+
+from data.config import Category, Role, Emoji
+from modules.utils import success_embed
+from modules.database import MemberModel
+
+
+    
+
+class ContactUsForm(discord.ui.Modal):
+    def __init__(self, client: discord.Client):
+        self.client = client
+
+        super().__init__(
+            title="Enter your message for contact us",
+            custom_id="contact_us_modal",
+            timeout=200
+        )
+        self.add_item(
+            TextInput(
+                style= discord.TextStyle.short,
+                label='Subject',
+                placeholder="What's your message subject? Be specific",
+                custom_id='contact_us_subject_input',
+                min_length = 5,
+                max_length = 120,
+                required = True,
+            )
+        )
+        self.add_item(
+            TextInput(
+                style= discord.TextStyle.long,
+                label='Message',
+                placeholder='Enter your message here...',
+                custom_id='contact_us_message_input',
+                min_length = 10,
+                max_length = 600,
+                required = True,
+            )
+        )
+    
+    async def on_submit(self, interaction: discord.Interaction):
+        subject = discord.utils.get(self.children, custom_id='contact_us_subject_input').value
+        message = discord.utils.get(self.children, custom_id='contact_us_message_input').value
+        guild = interaction.guild
+        member_model = await MemberModel.find_one(MemberModel.member_id == interaction.user.id)
+        category = await self.client.fetch_channel(Category.TICKETS)
+        user = await self.client.fetch_user(member_model.member_id)
+        ram_role = guild.get_role(Role.RAM)
+
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_messages=True),
+            ram_role: discord.PermissionOverwrite(read_messages=True, send_messages=False, manage_messages=True),
+            user: discord.PermissionOverwrite(read_messages=True, send_messages=True, )
+        }
+        ticket_channel = await category.create_text_channel(
+            name=f'{user.display_name}',
+            overwrites=overwrites,
+
+        )
+        em = discord.Embed(
+            title=f'{Emoji.CIRCLE} {subject}',
+            description=f'*{message}*\n\u200b',
+            color=discord.Color.blue()
+        )
+        em.set_thumbnail(url=user.avatar.url)
+        await ticket_channel.send(embed=em, content=f'{user.mention}')
+
+        await interaction.response.send_message(
+            embed = success_embed(f"Your ticket has been created successfully: {ticket_channel.mention}\n\n- Please be patient while team members handle this ticket."),
+            ephemeral=True
+        )
