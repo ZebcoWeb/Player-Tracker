aedc011bfc7e2dbf017289a1c37258ebbf353575
ZebcoWeb <zebco1382@gmail.com>
2022-07-20 23:01:08 +0430

    Update contexts

--- data/contexts.py
+++ data/contexts.py
@@ -1,30 +1,30 @@
 CONTEXT_LANG = {
     'title': '🌎 Select your language', 
-    'des': '- Choose your language from the **__%s__** available languages. Your desired language will be applied to other players joining your room.', 
+    'des': '▸ Choose your language from the **__%s__** available languages. Your desired language will be applied to other players joining your room.', 
 }
 
 CONTEXT_GAME = {
     'title': '🕹️ Choose a Game' , 
-    'des': "- Please choose the game you want to play from **__%s__** available games. If you don't find the game that you want to play from the list, let our team know in the <#855896054221373470> section so we can add it to the list.", 
+    'des': "▸ Please choose the game you want to play from **__%s__** available games. If you don't find the game that you want to play from the list, let our team know in the <#855896054221373470> section so we can add it to the list.", 
 }
 
 CONTEXT_ROOM_LIMIT = {
     'title':'👥 Select the room capacity',
-    'des':'- Please select the number of players that can enter you room. If you select <:infinityy:925130234876010568> option, the room will be created with unlimited capacity.',}
+    'des':'▸ Please select the number of players that can enter you room. If you select <:infinityy:925130234876010568> option, the room will be created with unlimited capacity.',}
 
 CONTEXT_ROOM_BITRATE = {
     'title':'🔊 Select bitrate' , 
-    'des':"- Please choose one of the bitrates provided below. The higher bitrate you choose, the sound will have more quality. The lower you choose, you'll use less internet data. We recommend using <:64kb:925140947610775562>",}
+    'des':"▸ Please choose one of the bitrates provided below. The higher bitrate you choose, the sound will have more quality. The lower you choose, you'll use less internet data. We recommend using <:64kb:925140947610775562>",}
 
 CONTEXT_ROOM_MODE = {
     'title':'🔐 Select room type', 
-    'des':'''Please choose one of the room types:\n- if you select <:unlockk:868207163993100339> **__Public__**, room information will be sent to `Tracker` channels for other players to see and enter your room.\n- If you select <:lockk:868207164420927497> **__Private__**, you'll receive an invitation link of your room, so you can let only specified people enter your room. They can enter your room via the link.''',}
+    'des':'''Please choose one of the room types:\n▸ if you select <:unlockk:868207163993100339> **__Public__**, room information will be sent to `Tracker` channels for other players to see and enter your room.\n▸ If you select <:lockk:868207164420927497> **__Private__**, you'll receive an invitation link of your room, so you can let only specified people enter your room. They can enter your room via the link.''',}
 
 CONTEXT_CONFIRM_ROOM = {
     'title': 'Your room information was successfully registered!',
-    'des': '''- You room information has successfully registered and you can confirm your selections by clicking on <:createe:868207164001497158> **__Confirm__** button.
-- If you think you have selected something wrongly, you can start over by selecting the <:againn:868207163569471570> **__Again__** button.
-- if you have changed your mind and don't want to create a new room, select the <:cancell:868207163582083083> **__Cancel__** button.''',}
+    'des': '''▸ You room information has successfully registered and you can confirm your selections by clicking on <:createe:868207164001497158> **__Confirm__** button.
+▸ If you think you have selected something wrongly, you can start over by selecting the <:againn:868207163569471570> **__Again__** button.
+▸ if you have changed your mind and don't want to create a new room, select the <:cancell:868207163582083083> **__Cancel__** button.''',}
 
 CONTEXT_CREATED_ROOM = {
     'title':'Your room was successfully created!',
@@ -35,7 +35,7 @@ CONTEXT_CREATED_ROOM = {
 
 CONTEXT_CANCEL_ROOM = {
     'title': '⚠️ Cancel room',
-    'des': '- Do you really want the room to be canceled?',
+    'des': '▸ Do you really want the room to be canceled?',
 }
 
 CONTEXT_CLOSE_ROOM = {
